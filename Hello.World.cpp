8d8a2adbdc604f16ec01da9c23ed66b1cb6fe057
Seba <tigersoft@wp.pl>
2016-02-22 15:50:50 +0100

    Added files via upload

---
+++ Hello.World.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+	cout << "HELLO WORLD" << endl; 
+	cout << "To jest moj pierwszy program \nnapisany w c++\n\n\n";
+	system ("pause");
+	return 0;
+}
